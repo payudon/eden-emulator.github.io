1b28218cf40b630fa224a720982d7d29a4bc3ec7
Bix <114880614+Bixbr@users.noreply.github.com>
2026-03-16 22:09:39 +0000

    Update download URLs and version to v0.2.0-rc2

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -14,8 +14,8 @@ function env(): Env {
     DISCORD_URL: import.meta.env.VITE_DISCORD_URL || 'https://discord.gg/4RtpRVWXcy',
     DOWNLOAD_URL:
       import.meta.env.VITE_DOWNLOAD_URL ||
-      'https://git.eden-emu.dev/eden-emu/eden/releases/tag/v0.2.0-rc1',
-    DOWNLOAD_VERSION: import.meta.env.VITE_DOWNLOAD_VERSION || 'v0.2.0-rc1',
+      'https://git.eden-emu.dev/eden-emu/eden/releases/tag/v0.2.0-rc2',
+    DOWNLOAD_VERSION: import.meta.env.VITE_DOWNLOAD_VERSION || 'v0.2.0-rc2',
     EMUREADY_API_BASE_URL: import.meta.env.DEV
       ? '/api/mobile/trpc'
       : import.meta.env.VITE_EMUREADY_API_BASE_URL || 'https://www.emuready.com/api/mobile/trpc',
